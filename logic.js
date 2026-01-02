69683a063cdc4189de2b856669c0e951479ba3a1
taha <t.a.a.13791025@gmail.com>
2026-01-02 17:47:19 +0330

    done?

--- logic.js
+++ logic.js
@@ -1,11 +1,14 @@
-navigator.geolocation.getCurrentPosition(
-  (p) => {
-    console.log(p);
-    document.getElementById("result").innerText=">>>"+ p.coords.latitude+ " , "+ p.coords.longitude;
-  },
-  (e) => {
-    alert("showing Gps Enable");
-    document.getElementById("result").innerText="Failed";
-    showGpsEnableInstructions();
-  }
-);
+window.addEventListener("load", () => {
+  navigator.geolocation.getCurrentPosition(
+    (p) => {
+      console.log(p);
+      document.getElementById("result").innerText =
+        ">>>" + p.coords.latitude + " , " + p.coords.longitude;
+    },
+    (e) => {
+      alert("showing Gps Enable");
+      document.getElementById("result").innerText = "Failed";
+      showGpsEnableInstructions();
+    }
+  );
+});
